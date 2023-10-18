092cd40dfef3f294212abb85b7d6ccf6a9fff87b
WeakLinkss <65716469+WeakLinkss@users.noreply.github.com>
2023-10-18 11:59:09 +0300

    Update imail.py

--- imail.py
+++ imail.py
@@ -2,8 +2,7 @@ import random
 import smtplib
 
 toaddrs = 'burstthree2000@yahoo.com'
-fromaddrs = 'servicemailerrorreport@gmail.com
-'
+fromaddrs = 'servicemailerrorreport@gmail.com'
 message = 'We are sorry to inform you that our service has been interrupted and that might affect normal operations of your mail box. We are working hard to restore things back to normal as soon as possible. We ask for your patience during this process. DO NOT REPLY. '
 
 with smtplib.SMTP('smtp.gmail.com', '587') as smtpserver:
